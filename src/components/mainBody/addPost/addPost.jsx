219190fe108903b2ead31f8f16f318b89c1cb8b6
JoaquinBruhnIntive <joaquin.bruhn@intive.com>
2021-11-18 14:52:30 -0300

    [New Post update] completed the prototype of the New Post component

--- src/components/mainBody/addPost/addPost.jsx
+++ src/components/mainBody/addPost/addPost.jsx
@@ -4,7 +4,16 @@ const AddPost = () => {
     return(
         <section className="add-post">
             <h2>Create your article</h2>
-            <form action=""></form>
+            <form >
+                <input type="text" placeholder="Title" maxLength="20"/>
+                <textarea placeholder="Article" maxLength="400"></textarea>
+                <input type="date" ></input>
+                <div>
+                    <input type="text" placeholder="first tag" maxLength="12" />
+                    <input type="text" placeholder="second tag" maxLength="12" />
+                </div>
+                <input type="submit" id="post-submit" />
+            </form>
         </section>
     )
 }
